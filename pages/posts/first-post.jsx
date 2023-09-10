6c6e54cfb67df35a8f906ae01ca17c636bb45423
AustinHuynh248 <huynhtransongngoc@gmail.com>
2023-09-10 17:12:40 +0700

    feat: meta style and css

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,25 +1,24 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
-      <div>
-        <h1>Welcome this is a new post</h1>
-        <Image
-          width="144"
-          height="144"
-          src="/images/profile.jpg"
-          alt="Profile pic"
-        />
-        <h2>
-          <Link href="/">← Back to home</Link>
-        </h2>
-      </div>
-    </>
+      <h1>Welcome this is a new post</h1>
+      <Image
+        width="144"
+        height="144"
+        src="/images/profile.jpg"
+        alt="Profile pic"
+      />
+      <h2>
+        <Link href="/">← Back to home</Link>
+      </h2>
+    </Layout>
   );
 }
